bd3ad5f3994f73e2b3de75f7a487232065dd62fc
otaku9820 <aguido@unal.edu.co>
2023-10-15 11:59:31 -0500

    Update futbolista.py

--- futbolista.py
+++ futbolista.py
@@ -45,4 +45,4 @@ class Futbolista(Deportista, Persona):
         
         
     def __str__(self):
-        return f"Mi nombre es {self.getNombre()}, soy profesional en el deporte {self.getDeporte()}, tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte."
+        return f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
